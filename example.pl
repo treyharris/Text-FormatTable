5105e62bc889c0a74e8a24c864d79adc3a63de67
Ricardo Signes <rjbs@semiotic.systems>
2023-06-25 11:51:49 -0400

    example.pl: add a second example, with color
    This demonstrates that color codes are ignored for width.
    
    It also demonstrates a current bug: that color codes leak from
    column to column.

--- example.pl
+++ example.pl
@@ -1,10 +1,35 @@
+use strict;
+use warnings;
+
 use lib 'lib';
+
+use Term::ANSIColor;
 use Text::FormatTable;
-my $table = Text::FormatTable->new('r|l');
-$table->head('a', 'b');
-$table->rule('=');
-$table->row('this a test, a nice test', 'a test!');
-$table->rule;
-$table->row('you mean it\'s really a test?', 'yep');
-$table->rule('=');
-print $table->render(20);
+
+{
+  my $table = Text::FormatTable->new('r|l');
+  $table->head('a', 'b');
+  $table->rule('=');
+  $table->row('this a test, a nice test', 'a test!');
+  $table->rule;
+  $table->row('you mean it\'s really a test?', 'yep');
+  $table->rule('=');
+  print $table->render(20);
+}
+
+print "\n", "-" x 78, "\n\n";
+
+
+{
+  my $table = Text::FormatTable->new('r|l');
+  $table->head('a', 'b');
+  $table->rule('=');
+  $table->row(
+    'this a ' . colored(['bright_blue'], 'test, a nice') . ' test',
+    'a test you can count on!',
+  );
+  $table->rule;
+  $table->row('you mean it\'s really a test?', 'yep');
+  $table->rule('=');
+  print $table->render(20);
+}
